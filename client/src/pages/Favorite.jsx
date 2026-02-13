e414a34456f80be9abf57ae8b32f7d92a38e2ca2
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-02-13 22:58:41 +0530

    movieDetails

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { dummyShowsData } from '../assets/assets'
+import MovieCard from '../components/MovieCard'
+import BlurCircle from '../components/BlurCircle'
 
 const Favorite = () => {
-  return (
-    <div>
+  return dummyShowsData.length > 0 ? (
+    <div className='relative pt-28 pb-20 px-6 md:px-16 lg:px-24 xl:px-32 min-h-[80vh]'>
       
+
+      <BlurCircle top="150px" left="0px"/>
+      <BlurCircle buttom="50px" right="50px"/>
+      <h1 className='text-2xl font-semibold mb-8'>Your Favorite Movies</h1>
+
+      <div className='grid 
+        grid-cols-2 
+        sm:grid-cols-2 
+        md:grid-cols-3 
+        lg:grid-cols-4 
+        xl:grid-cols-4 
+        gap-8'>
+        
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
+
+    </div>
+  ) : (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-3xl font-bold text-center'>No Movies Available</h1>
     </div>
   )
 }

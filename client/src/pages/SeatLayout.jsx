c3db6bf288d63700aa39dcca9346414bd9e9ac2b
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-02-22 23:14:32 +0530

    done

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, {useEffect, useState} from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import { dummyDateTimeData, dummyShowsData, ClockIcon } from '../assets/assets'
 import Loading from '../components/Loading'
+import isoTimeFormat from '../lib/isoTimeFormat'
 
 const SeatLayout = () => {
 
@@ -23,6 +26,7 @@ const SeatLayout = () => {
   }
 
 useEffect(()=>{
+    // eslint-disable-next-line react-hooks/set-state-in-effect
    getShow()
 },[])
 
@@ -33,15 +37,23 @@ useEffect(()=>{
       <div className='w-60 bg-primary/10 border border-primary/20 rounded-lg py-10
       h-max md:sticky md:top-30'>
         <p className='text-lg font-semibold px-6'>Available Timings</p>
-        <div>
-          {show.dateTime[date].map((item)=>(
-             <div className='flex items-center gap-2 px-6 py-2 w-max rounded-r-md 
-             cursor-pointer tranistion'>
-              <ClockIcon className="w-4 h-4"/>
-              <p className='text-sm'>{item.time}</p>
-             </div>
-          ))}
-        </div>
+        <div className='mt-5 space-y-1'>
+  {show?.dateTime?.[date]?.map((item,index)=>(
+    <div
+      key={index}
+      onClick={()=>setSelectedTime(item)}
+      className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md 
+      cursor-pointer transition ${
+        selectedTime?.time === item.time
+          ? "bg-primary text-white"
+          : "hover:bg-primary/20"
+      }`}
+    >
+      <ClockIcon className="w-4 h-4"/>
+      <p className='text-sm'>{isoTimeFormat(item.time)}</p>
+    </div>
+  ))}
+</div>
       </div>
     
       {/* {Seat Layout} */}

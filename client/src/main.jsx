35afd0d9d756be70db87f866a8f3086d9f39b623
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-02-26 14:47:31 +0530

    Booking page done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,27 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { ClerkProvider } from '@clerk/clerk-react'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY =
+  import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-const AppTree = () => (
-   <BrowserRouter>
-      <App />
-   </BrowserRouter>
-)
+const RootApp = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
-createRoot(document.getElementById('root')).render(
-   PUBLISHABLE_KEY ? (
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-         <AppTree />
-      </ClerkProvider>
-   ) : (
-      <AppTree />
-   )
-)
+createRoot(document.getElementById("root")).render(
+  PUBLISHABLE_KEY ? (
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl="/"
+    >
+      <RootApp />
+    </ClerkProvider>
+  ) : (
+    <RootApp />
+  )
+);

45589beda21361eefe44c74d87dee133d8e8c2da
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-03-24 09:50:56 +0530

    Dashboard

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -24,9 +24,17 @@ const AdminSidebar = () => {
     <p className='pt-2 text-base max:md:hidden'>{user.firstName}{user.LastName}</p>
     <div className='w-full'>
       {adminNavlink.map((link,index)=>(
-         <NavLink className={({isActive})=>`relative flex items-center 
+         <NavLink key={index} to={link.path} className={({isActive})=>`relative flex items-center 
          max-md:justify-center gap-2 w-full py-2.5 md:pl-10 first:mt-6
          text-gray-400 ${isActive && 'bg-primary/15 text-primary group'}`}>
+          {({ isActive })=>(
+            <>
+             <link.icon className="w-5 h-5"/>
+             <P className="max-md:hidden">{link.name}</P>
+             <span className={`w-1.5 h-10 rounded-1 right-0 absolute $
+              {isActive && 'bg-primary'}`}/>
+            </>
+          )}
 
          </NavLink>
       ))}
--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { CircleDollarSignIcon, SquareBottomDashedScissors, UsersIcon } from 'lucide-react';
+import React, { useEffect } from 'react'
+import { dummyDashboardData } from '../../assets/assets';
 
 const Dashboard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [dashboardData,setDashboardData] = useState({
+    totalBookings:0,
+    totalRevenue:0,
+    activeShows:[],
+    totalUser:0
+  });
+
+  const [loading,setLoading] = useState(true);
+
+  const dashboradCards = [
+    {title:"Total Bookings",value: dashboardData.totalBookings || "0",icon:chartLineIcon},
+    {title:"Total Revenue", value: dashboardData.totalRevenue || "0",icon:CircleDollarSignIcon},
+    {title:"Active Shows",value:dashboardData.activeShows.length || "0", icon:UsersIcon},
+    {title:"Total Users",value:dashboardData.totalUser || "0",icon: UserIcon}
+  ]
+
+  const fetchDashboardData = async () =>{
+    setDashboardData(dummyDashboardData)
+    setLoading(false)
+  };
+
+  useEffect(()=>{
+    fetchDashboardData();
+  },[]);
 }
 
 export default Dashboard

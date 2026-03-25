312c402a7530f6bfdb2aaafe59b90f643fac744b
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-03-25 23:14:35 +0530

    Done

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { CircleDollarSignIcon, SquareBottomDashedScissors, UsersIcon } from 'lucide-react';
+import { CircleDollarSignIcon, SquareBottomDashedScissors, StarIcon, UsersIcon } from 'lucide-react';
 import React, { useEffect } from 'react'
 import { dummyDashboardData } from '../../assets/assets';
 import Title from './Title';
@@ -53,6 +53,34 @@ const Dashboard = () => {
 
         </div>
           <p className="mt-10 text-lg font-medium">Active Shows</p>
+
+          <div className="relative flext flex-wrap gap-6 mt-4 max-w-5xl">
+            <BlurCircle top="100px" left="-10px"/>
+            {dashboardData.activeShows.map((show))=>(
+              <div key={show._id} className="w-55 rounded-lg overflow-hidden 
+              h-full pb-3 bg-primary/10 border border-primary/20
+              hover:-translate-y-1 transition duration-300">
+                <img src={show.movie.poster_path} alt="" className="h-60 w-full
+                object-cover" />
+
+                <p className="font-medium p-2 truncate">{show.movie.title}
+                </p>
+                <div className="flex items-center justify-between px-2">
+                  <p className="text-lg font-medium">{currency}{show.showPrice}</p>
+                  <p className="flex items-center gap-1 text-sm 
+                  text-gray-400 mt-1 pr-1">
+
+                  <StarIcon className="w-4 h-4 text-primary
+                  full-primary"/>
+                  {show.movie.vote_average.toFixed(1)}
+                  </p>
+
+                </div>
+               <p className="px-2 pt-2 text-sm text-gray-500">{dateFormat(show.showDateTime)}</p>
+
+              </div>
+            )}
+          </div>
        </>
   ) : <Loading />
 }

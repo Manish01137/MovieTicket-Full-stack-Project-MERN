88ed7ca59758870e45631feab72a1d635cbb4623
Manish01137 <manishBTECH23@ced.alliance.edu.in>
2026-03-29 13:15:05 +0530

    List Booking page done

--- client/src/pages/admin/ListBooking.jsx
+++ client/src/pages/admin/ListBooking.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../../assets/assets';
+import Loading from '../../components/Loading';
+import Title from './Title';
 
 const ListBooking = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const currency = import.meta.env.VITE_CURRENCY
+  const [bookings,setBookings] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
+
+  const getAllBookings = async () =>{
+    setBookings(dummyBookingData)
+    setIsLoading(false);
+  };
+
+  useEffect(()=>{
+    getAllBookings();
+  },[]);
+  return !isLoading ?(
+     <>
+     <Title text1="List" text2="Bookings" />
+
+      <div className="max-w-4xl mt-6 overflow-x-auto">
+        <table className="w-full border-collaps rounded-md overflow-hidden text-nowrap">
+           <tr className="bg-primary/20 text-left text-white">
+           <th className="p-2 font-medium pl-5">User Name</th>
+           <th className="p-2 font-medium">Movie Name</th>
+           <th className="p-2 font-medium">Show Time</th>
+           <th className="p-2 font-medium">Seats</th>
+           <th className="p-2 font-medium">Amount</th>
+             
+           </tr>
+        </table>
+      </div>
+     </>
+  ): <Loading/>
 }
 
 export default ListBooking
